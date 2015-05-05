f64be6ed2a62f6c1cc7d7594f396e6bf89a65e29
FrankHXW <FrankHXW@gmail.com>
2015-05-05 23:41:34 +0800

    commit examples of chapter2_1 and chapter2_3

--- Learning/learning/learning2/main.cpp
+++ Learning/learning/learning2/main.cpp
@@ -1,10 +1,14 @@
-#include "learning2.h"
 #include <QtWidgets/QApplication>
+#include "2.1_finddialog.h"
+#include "2.3_gotocelldialog.h"
 
 int main(int argc, char *argv[])
 {
-	QApplication a(argc, argv);
-	learning2 w;
-	w.show();
-	return a.exec();
-}
+	QApplication app(argc, argv);
+	FindDialog *dialog1 = new FindDialog;
+	dialog1->show();
+
+	GoToCellDialog *dialog2 = new GoToCellDialog;
+	dialog2->show();
+	return app.exec();
+} 
