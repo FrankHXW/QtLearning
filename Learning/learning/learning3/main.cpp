9720d4950c67fa799dce417ebac0f510f233da11
FrankHXW <FrankHXW@gmail.com>
2015-08-13 11:19:17 +0800

    finish GUI

--- Learning/learning/learning3/main.cpp
+++ Learning/learning/learning3/main.cpp
@@ -1,7 +1,9 @@
 #include <QtWidgets/QApplication>
 #include<QLabel>
 #include<QComboBox>
-#include <QStyleFactory>
+#include<QStyleFactory>
+#include<QSplashScreen>
+
 
 #include"mainwindow.h"
 
@@ -10,7 +12,17 @@ int main(int argc, char *argv[])
 	QApplication::setStyle("Fusion");
 	QApplication app(argc, argv);
 
+	QSplashScreen *splash = new QSplashScreen;
+	splash->setPixmap(QPixmap("./images/splash.jpg"));
+	splash->show();
+
+	Qt::Alignment bottomLeft = Qt::AlignBottom | Qt::AlignLeft;
+	splash->showMessage(QObject::tr("Setting up the main window...."), bottomLeft, Qt::white);
+
 	MainWindow *mainWin=new MainWindow;
 	mainWin->show();
+
+	splash->finish(mainWin);
+	delete splash;
 	return app.exec();
 }
